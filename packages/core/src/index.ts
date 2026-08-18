11f811b165a219a2a4fc68464a52ceffb7a7dafb
UnknownGod2011 <tanushshah2006@gmail.com>
2026-08-19 02:09:17 +0530

    Add guarded human resolution claims

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -227,6 +227,7 @@ export interface VerificationEngine {
 export * from "./coordinator.js";
 export * from "./errors.js";
 export * from "./execution.js";
+export * from "./human-resolution.js";
 export * from "./memory.js";
 export * from "./run-finalization.js";
 export * from "./run-state.js";
